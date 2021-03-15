aed4b5d34f808054acde7119854c25b756cc5678
kiplangat15000 <kiplangatdennis99@gmail.com>
2021-03-15 08:05:03 +0300

    Assigning variables values

---
+++ script.js
@@ -0,0 +1,12 @@
+function calculate() {
+    var shedA = document.getElementById("milk-a").value;
+    var shedB = document.getElementById("milk-b").value;
+    var shedC = document.getElementById("milk-c").value;
+    var shedD = document.getElementById("milk-d").value;
+
+    var price = 45;
+    var day = 7;
+    var monthD = 30;
+    var yearly = 12;
+    var con = 1;
+}
