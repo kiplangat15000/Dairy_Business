3aea57d447a3cf0ebe1db9c4efc746769532c057
kiplangat15000 <kiplangatdennis99@gmail.com>
2021-03-15 08:08:16 +0300

    Adding objects inside the first function

--- script.js
+++ script.js
@@ -20,4 +20,23 @@ function calculate() {
 
     var resultM = (((shedA * price) + (shedB * price) + (shedC * price) + (shedD * price))*day);
     document.getElementById("weeklyincome").innerHTML = "Total income per week is Ksh " + resultM;
+
+
+    function calculate2() {
+        var leepYear = [
+            { value: "January", days: 31 },
+            { name: "February", days: 29 },
+            { name: "March", days: 31 },
+            { name: "April", days: 30 },
+            { name: "May", days: 31 },
+            { name: "June", days: 30 },
+            { name: "July", days: 31 },
+            { name: "August", days: 31 },
+            { name: "September", days: 30 },
+            { name: "October", days: 31 },
+            { name: "November", days: 30 },
+            { name: "December", days: 31 },
+            
+        ]
+    }
 }
